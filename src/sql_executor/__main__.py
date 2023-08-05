040c0d9ab94c0a785d0e134bddfe71622af4517e
Naoya Yamashita <conao3@gmail.com>
2023-08-05 16:31:39 +0900

    implement simple sa

--- src/sql_executor/__main__.py
+++ src/sql_executor/__main__.py
@@ -1,2 +1,18 @@
+import argparse
+import sqlalchemy as sa
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', help='DB URL like postgresql://{username}:{password}@{host}:{port}/{database}', required=True)
+
+    return parser.parse_args()
+
+
 def main() -> None:
-    print('hello')
+    args = parse_args()
+
+    engine = sa.create_engine(args.url)
+    with engine.connect() as conn:
+        result = conn.execute(sa.text('SELECT 1'))
+        print(result.fetchone())
